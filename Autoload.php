44caf144f58c63269c0c0e52e430707c3d3b0e9e
unknown <bintaagungp@gmail.com>
2020-02-01 14:25:12 +0700

    modified at 02/01/2020

--- Autoload.php
+++ Autoload.php
@@ -1,12 +1,3 @@
 <?php
 
 require(__DIR__."/core/Loader.php");
-
-$PHPTemplate = new PHPTemplate();
-
-$PHPTemplate->template("template/template");
-$PHPTemplate->content("content/content");
-$PHPTemplate->component("component/component");
-$PHPTemplate->data(['description' => 'This is example that i make to entertain you how to understand the flow of PHPTemplate. Make it easier with PHPTemplate. Enjoy :)']);
-
-$PHPTemplate->view();
